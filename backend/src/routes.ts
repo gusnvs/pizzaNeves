15896d39968d498139fb5e3d8152596911cc13c3
gusnvs <gagneves@hotmail.com>
2023-03-04 14:02:18 -0400

    Configuring the handling of possible errors, with express-async-errors and enabling cors so that any IP can make API requests

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,7 +3,8 @@ import { Router, Request, Response } from "express";
 const router = Router();
 // agora podemos criar uma rota da minha API
 router.get('/teste', (req: Request, res: Response) => {
-    return res.json({ nome: 'Pizza Neves' })
+    //throw new Error('Erro ao fazer essa requisição!');
+    return res.json({ nome: 'Pizza Neves' });
 });
 
 export { router };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,26 @@
 import express, { Request, Response, NextFunction } from "express";
+import 'express-async-errors'; // essa biblioteca foi importada no intuito de tratar erros
+import cors from 'cors';
 import { router } from "./routes";
 
 const app = express(); // Estamos colocando tudo o que o express tem a oferecer dentro do app
+
 app.use(express.json()); // Estou falando para o express que o tipo de formato que vamos usar é o json
+
+app.use(cors()); // Estamos habilitando para que qualquer URL,IP conseguir fazer essa requisição na nossa API, não queremos bloqieo dela
+
 app.use(router); // Estou falando que as rotas que minha aplicação vão ter, está dentro do router
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => { // vamos tratar dois tipos de erro que possa acontecer
+    if (err instanceof Error) {
+        // Se for uma instância do tipo Error vamos lançar uma excessão 
+        return res.status(400).json({
+            error: err.message
+        });
+    }
+    // Se for outro tipo de Error
+    return res.status(500).json({status: 'error', message: 'Internal Server Error'});
+
+}); // vamos criar a barreira através de um midway, conforme a biblioteca pede
+
 app.listen(3333, () => console.log('Servidor Online!')); // vamos inicializar o projeto na porta que eu quero, com um callback de informação
