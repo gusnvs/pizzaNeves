654eb46bc9abab0562ab0ee4076ea1caa6ca4cf8
gusnvs <gagneves@hotmail.com>
2023-03-06 10:54:48 -0400

    We used the JWT to generate the token, and we did the validation for when the user enters the system, the tokens being generated.

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -2,6 +2,7 @@
 
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface AuthRequest {
     email: string;
@@ -11,10 +12,10 @@ interface AuthRequest {
 class AuthUserService {
 
     async execute({ email, password }: AuthRequest) {
-       
+
         // Vamos verificar esse usuário
 
-        if(!email || !password) {
+        if (!email || !password) {
             throw new Error("Este campo é obrigatório!");
         }
 
@@ -24,19 +25,41 @@ class AuthUserService {
             }
         })
 
-        if(!authAlreadyExist) {
+        if (!authAlreadyExist) {
             throw new Error("E-mail ou senha está incorreto!");
         }
 
         const passwordCompareExist = await compare(password, authAlreadyExist.password);
 
-        if(!passwordCompareExist) {
+        if (!passwordCompareExist) {
             throw new Error("E-mail ou senha está incorreto!");
         }
 
         // Se passou por toda a verificação, vamos gerar um token JWT e devolver os dados do usuário como ID, nome e senha
 
-        return { ok: true };
+        const token = sign(
+            {
+                name: authAlreadyExist.name,
+                email: authAlreadyExist.email
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: authAlreadyExist.id,
+                expiresIn: '10d'
+            }
+            )
+        // A função sign exige um parâmetro senha
+        // Vamos gerar um hash, e salvar numa variável de ambiente, pois não é interessante a senha ficar exposta
+        // Usamos o dotenv para acessar as variáveis de ambiente
+
+
+
+        return {
+            id: authAlreadyExist.id,
+            name: authAlreadyExist.name,
+            email: authAlreadyExist.email,
+            token: token
+        };
     }
 }
 
